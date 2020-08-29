247a839d6bb47686e3c6b44550a87cd873b06f5b
Vishal Thakur <39324895+vishal219@users.noreply.github.com>
2020-08-29 10:08:26 -0700

    initial files added

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Header';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
+import Dasboard from './components/Dasboard';
+// import India from './components/India'
+
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={Dasboard}></Route>
+        {/* <Route path="/india" component={India}></Route> */}
+      </Switch>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
